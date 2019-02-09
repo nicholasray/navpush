aac1647272e22deef0ef0eed1b84aeadc5579737
Nick Ray <nray@nray.io>
2019-02-08 17:41:32 -0700

    Remove bottom as an option
    It was causing too many problems due to ios safaris bottom menu bar and
    its landscape mode

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import {
   top as FullTop,
-  bottom as FullBottom,
   left as FullLeft,
   right as FullRight
 } from './strategy/full';
 import {
   top as OverlayTop,
-  bottom as OverlayBottom,
   left as OverlayLeft,
   right as OverlayRight
 } from './strategy/overlay';
 import {
   top as PushTop,
-  bottom as PushBottom,
   left as PushLeft,
   right as PushRight
 } from './strategy/push';
 import {
   top as UncoverTop,
-  bottom as UncoverBottom,
   left as UncoverLeft,
   right as UncoverRight
 } from './strategy/uncover';
@@ -29,17 +25,13 @@ export {
   FullLeft,
   FullRight,
   FullTop,
-  FullBottom,
   OverlayLeft,
   OverlayRight,
   OverlayTop,
-  OverlayBottom,
   PushLeft,
   PushRight,
   PushTop,
-  PushBottom,
   UncoverLeft,
   UncoverRight,
-  UncoverTop,
-  UncoverBottom
+  UncoverTop
 };
--- src/strategy/uncover/index.js
+++ src/strategy/uncover/index.js
@@ -1,6 +1,5 @@
 import top from './top';
-import bottom from './bottom';
 import left from './left';
 import right from './right';
 
-export { top, bottom, left, right };
+export { top, left, right };
