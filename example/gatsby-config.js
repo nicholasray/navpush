a113ba9483a47f3d3a4009bbe3dd519cb8776129
Nick Ray <nray@nray.io>
2019-02-17 22:27:35 -0700

    Update primary color

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -38,8 +38,8 @@ module.exports = {
         name: 'navpush',
         short_name: 'navpush',
         start_url: '/',
-        background_color: '#345366',
-        theme_color: '#345366',
+        background_color: '#275875',
+        theme_color: '#275875',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
